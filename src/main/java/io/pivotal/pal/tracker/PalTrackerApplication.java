e41272a492e2d9ee3c7cbef00509f9432c752031
ubuntu <jvmonisha@gmail.com>
2021-01-20 20:09:27 +0000

    Spring JDBC template

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,7 +1,13 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class PalTrackerApplication {
@@ -9,4 +15,11 @@ public class PalTrackerApplication {
     public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
+
+    @Bean
+    @Primary
+    public JdbcTimeEntryRepository jdbcTimeEntryRepository(DataSource dataSource){
+        return new JdbcTimeEntryRepository(dataSource);
+    }
+
 }
